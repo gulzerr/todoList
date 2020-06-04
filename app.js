c819d3b2ed2f9e956752fa312f8d493d5a62b242
duranta <gulzer.deb@gmail.com>
2020-06-04 21:53:10 +0600

    A full working todo list app

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const todoController = require('./controllers/todoController');
+
+const port = 3000;
+
+const app = express();
+
+//template engine
+app.set('view engine', 'ejs');
+
+//static files
+app.use(express.static('./public'));
+
+//fire  controllers
+todoController(app);
+
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER
+        }:${process.env.MONGO_PASSWORD
+        }@cluster0-0aum2.mongodb.net/${process.env.MONGO_DB
+        }?retryWrites=true&w=majority`
+    ).then(() => {
+    app.listen(port,function(){
+        console.log('app listnening on port '+ port);
+    });
+    }).catch(err => {
+    console.log(err);
+    }
+);
+
---
+++ controllers/todoController.js
@@ -0,0 +1,52 @@
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// var data = [{item:'Get milk'}, {item:'Workout'}, {item:'Coding'}]
+var urlEncodedParser = bodyParser.urlencoded({extended: false});
+
+var todoSchema = new mongoose.Schema({
+    item: String
+})
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+// var itmeOne = new Todo({item : 'buy flowers'}).save(function(err){
+//     if(err) throw err;
+//     console.log('item saved');
+// });
+
+module.exports = function(app){
+    app.get('/todo', function(req,res){
+        // get data from mongo
+        Todo.find({}, function(err, data){
+            if(err) throw err;
+            res.render('todo', {todos: data});
+        })
+    });
+
+    app.post('/todo',urlEncodedParser, function(req,res){
+        // get from mong and add
+        var newTodo = Todo(req.body).save(function(err, data){
+            if(err) throw err;
+            res.json(data);
+        })
+    });
+
+    app.delete('/todo/:item', function(req,res){
+        // remove item from mongodb
+        var sub = req.params.item.replace(/\-/g," ");
+        sub = sub.substring(1,sub.length-1);
+        console.log(sub.length);
+        console.log('fuck');
+        Todo.deleteOne({item: sub}, function (err, data) { 
+            if (err) console.log(err);;
+            res.send(data);
+         })
+
+        // data = data.filter(function(todo){
+        //     console.log(`-${todo.item.replace(/ /g, '-')}-`, `-${todo.item.replace(/ /g, '-')}-` != req.params.item)
+        //     return `-${todo.item.replace(/ /g, '-')}-` != req.params.item;
+        // });
+        // res.json(data);
+    });
+}
